31f7e44e87f8531ceb39352065afdddebdf82c9a
Maxon <makc-corp@yandex.ru>
2019-09-13 10:57:19 +0300

    Библиотека для подсчета площадей + тестовая программа

---
+++ Square/Square/calculate.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+using System.Windows.Forms;
+
+
+
+
+
+namespace Square
+{
+    public class calculate
+    {
+        public double CircleSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        public double TriangleSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public double Square(params double[] values)
+        {
+            int arguments = values.Length;
+
+            if (arguments > 0)
+            {
+                int i = 0;
+                do
+                {
+                    if (values[i] == 0)
+                    {
+                        MessageBox.Show("Arguments can't be '0'");
+                        return -1;
+                    }
+                    i++;
+                }
+                while (i < arguments);
+            }
+            else
+            {
+                MessageBox.Show("No arguments");
+                return -1;
+            }
+
+            if (arguments == 1)
+            {
+                return CircleSquare(values[0]);
+            }
+            else if (arguments == 3)
+            {
+                double a = values[0];
+                double b = values[1];
+                double c = values[2];
+
+                if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a))
+                {
+                    MessageBox.Show("Triangle is right");
+                }
+                return TriangleSquare(a, b, c);
+            }
+            else
+            {
+                MessageBox.Show("No suitable formulas");
+                return -1;
+            }
+        }
+
+    }
+}
---
+++ TestSquare/TestSquare/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Square;
+
+namespace TestSquare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        calculate calc = new calculate();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+//                 double i = calc.Area(2.0);
+                double i = calc.Square();
+//                 double i = calc.CircleArea(2.0);
+//                 double i = calc.TriangleArea(6.0, 4.0, 8.0);
+
+                txtResult.Text = i.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
